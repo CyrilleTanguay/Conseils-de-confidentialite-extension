66c68fb9b25b1b194f7931142e4747557857eb57
CyrilleTanguay <e1974422@cmaisonneuve.qc.ca>
2023-02-13 23:55:29 -0500

    Remaniement des liens

--- browserAction/MenuPr/ScriptsParSujet/Ordi.js
+++ browserAction/MenuPr/ScriptsParSujet/Ordi.js
@@ -1,7 +1,37 @@
+//window.onload = function () {
+
+  var BtnOrdi= document.getElementById("VotreOrdi");
+
+
+  if (BtnOrdi){
+  BtnOrdi.addEventListener("click", Ordi); 
+//}
+
+
 function Ordi(){
-    document.getElementById('laListe').innerHTML="<p onclick='ouvrirAjust()'>Ajustements</p><div id='Ajust'><ul><li>Vérifiez les paramètres de sécurité de votre ordinateur et désactivez la collecte de données.</li><li>Faites la mise à jour du système si une nouvelle version est disponible. Les mises à jour contiennent des correctifs permettant de rendre le système moins vulnérable.</li><li>Désactivez la mise à jour automatique (pour Windows). Bien que faire une mise à jour du système soit primordial pour notre sécurité, elle peut survenir au mauvais moment.</li><li>Enlevez des connections tiers avec des applications.</li><li>Activez le pare-feu</li><li>Désactivez l’option « masquer les extensions de fichier ». Cela vous permettra de cerner des fichiers pirates.</li></ul></div><hr><p onclick='ouvrirInte()'>Internet</p><div id='Inte'><ul><li>Méfiez-vous des réseaux publics. Ils vous rendent plus vulnérables aux pirates. Ce n’est pas pour rien que lorsque vous choisissez ce réseau, il y a « réseau non sécurisé » qui est inscrit sur votre iPhone!</li><li>Désactivez Bluetooth si vous n’en avez pas besoin</li><li>Se connecter sur vos données cellulaires n’est pas plus sécuritaire. Vous pourriez être piraté par phishing ou par l’exploitation de vulnérabilités dans des appareils mobiles.</li><li>Si vous êtes sur un réseau public, n’allez pas sur les réseaux sociaux et ne vous connectez pas à un compte bancaire.</li></ul></div><hr><p onclick='ouvrirAppInte()'>Applications Internet</p><div id='appInte'><ul><li>Un antivirus. Comme son nom l’indique, il permet de détecter des fichiers malveillants.</li><li>Un VPN. Si comme moi vous passez du temps sur YouTube, vous savez sûrement ce que c’est. En gros, un VPN permet de générer une adresse IP, ce qui vous permettra de rendre votre emplacement secret. L’argument de vente principal d’un VPN est le fait qu’il vous permette de regarder des séries ou des films en streaming qui ne sont disponibles que dans un pays étranger.</li></ul></div>";
+    document.getElementById('laListe').innerHTML="<p id='ouvrirAjust'>Ajustements</p><div id='Ajust'><ul><li>Vérifiez les paramètres de sécurité de votre ordinateur et désactivez la collecte de données.</li><li>Faites la mise à jour du système si une nouvelle version est disponible. Les mises à jour contiennent des correctifs permettant de rendre le système moins vulnérable.</li><li>Désactivez la mise à jour automatique (pour Windows). Bien que faire une mise à jour du système soit primordial pour notre sécurité, elle peut survenir au mauvais moment.</li><li>Enlevez des connections tiers avec des applications.</li><li>Activez le pare-feu</li><li>Désactivez l’option « masquer les extensions de fichier ». Cela vous permettra de cerner des fichiers pirates.</li></ul></div><hr><p id='ouvrirInte'>Internet</p><div id='Inte'><ul><li>Méfiez-vous des réseaux publics. Ils vous rendent plus vulnérables aux pirates. Ce n’est pas pour rien que lorsque vous choisissez ce réseau, il y a « réseau non sécurisé » qui est inscrit sur votre iPhone!</li><li>Désactivez Bluetooth si vous n’en avez pas besoin</li><li>Se connecter sur vos données cellulaires n’est pas plus sécuritaire. Vous pourriez être piraté par phishing ou par l’exploitation de vulnérabilités dans des appareils mobiles.</li><li>Si vous êtes sur un réseau public, n’allez pas sur les réseaux sociaux et ne vous connectez pas à un compte bancaire.</li></ul></div><hr><p id='ouvrirAppInte'>Applications Internet</p><div id='appInte'><ul><li>Un antivirus. Comme son nom l’indique, il permet de détecter des fichiers malveillants.</li><li>Un VPN. Si comme moi vous passez du temps sur YouTube, vous savez sûrement ce que c’est. En gros, un VPN permet de générer une adresse IP, ce qui vous permettra de rendre votre emplacement secret. L’argument de vente principal d’un VPN est le fait qu’il vous permette de regarder des séries ou des films en streaming qui ne sont disponibles que dans un pays étranger.</li></ul></div>";
     document.querySelector("header").style.backgroundColor="red";
-    document.getElementById('myHeading').innerHTML="<div class='caseRetour' onclick='RetourPrincipal()'>Retour</div>Votre ordinateur";
+    document.getElementById('myHeading').innerHTML="<div id='caseRetour'>Retour</div>Votre ordinateur";
+console.log("oui");
+var BtnAjust= document.getElementById("ouvrirAjust");
+if (BtnAjust){
+  BtnAjust.addEventListener("click", ouvrirAjust); 
+}
+var BtnInte=document.getElementById("ouvrirInte");
+if(BtnInte){
+  BtnInte.addEventListener("click", ouvrirInte);
+}
+var BtnAppInte=document.getElementById("ouvrirAppInte");
+if(BtnAppInte){
+  BtnAppInte.addEventListener("click", ouvrirAppInte);
+}
+var BtnReviens= document.getElementById("caseRetour");
+if (BtnReviens){
+  BtnReviens.addEventListener("click", RetourPrincipal); 
+}
+
+}
+
 }
 
 function ouvrirAjust(){
--- browserAction/MenuPr/script.js
+++ browserAction/MenuPr/script.js
@@ -1,6 +1,23 @@
 function RetourPrincipal(){
-    document.getElementById('laListe').innerHTML='<p id="VotreOrdi" onclick="Ordi()">Votre ordinateur</p><hr><p onclick="AppNon()"> Applications à ne pas installer (index)</p><hr><p onclick="Navigateur()">Le Navigateur</p><hr><p onclick="MDP()">Le mot de passe</p><hr><p id="fiable" onclick="Fiable()">La fiabilité d’un site web</p><hr><p id="reseau" onclick="Reseaux()">Réseaux sociaux</p><hr><p id="extorsion" onclick="Extorsion()">Extorsion / Sextorsion / Chantage</p><hr><p id="sms" onclick="SMS()">Messagerie</p><hr><p id="achats" onclick="Achats()">Achats en ligne</p><hr><p id="achatApp" onclick="achatApp()">Achat d’application</p><hr><p id="politique" onclick="Politique()">La politique de confidentialité</p><hr><p id="virus" onclick="Virus()">Virus</p><hr><p id="attaques" onclick="Attaques()">Attaques</p><hr><p id="mobiles" onclick="Mobiles()">Conseils pour les téléphones mobiles</p>';
+    document.getElementById('laListe').innerHTML='<p id="VotreOrdi">Votre ordinateur</p><hr><p id="indexAppNon"> Applications à ne pas installer (index)</p><hr><p id="leNav" onclick="Navigateur()">Le Navigateur</p><hr><p id="leMDP" onclick="MDP()">Le mot de passe</p><hr><p id="fiable" onclick="Fiable()">La fiabilité d’un site web</p><hr><p id="reseau" onclick="Reseaux()">Réseaux sociaux</p><hr><p id="extorsion" onclick="Extorsion()">Extorsion / Sextorsion / Chantage</p><hr><p id="sms" onclick="SMS()">Messagerie</p><hr><p id="achats" onclick="Achats()">Achats en ligne</p><hr><p id="achatDeApp" onclick="achatApp()">Achat d’application</p><hr><p id="politique" onclick="Politique()">La politique de confidentialité</p><hr><p id="virus" onclick="Virus()">Virus</p><hr><p id="attaques" onclick="Attaques()">Attaques</p><hr><p id="mobiles" onclick="Mobiles()">Conseils pour les téléphones mobiles</p>';
     document.getElementById('myHeading').innerHTML="Liste des conseils";
     document.querySelector("header").style.backgroundColor="aqua";
     document.querySelector("main").style.fontFamily="Oswald, sans-serif";
+
+    
 }
+function startListen(){
+    if(document.getElementById("indexAppNon")){
+document.getElementById("indexAppNon").addEventListener("click", AppNon);}
+document.getElementById("leNav").addEventListener("click", Navigateur);
+document.getElementById("leMDP").addEventListener("click", MDP);
+document.getElementById("fiable").addEventListener("click", Fiable);
+document.getElementById("reseau").addEventListener("click", Reseaux);
+document.getElementById("extorsion").addEventListener("click", Extorsion);
+document.getElementById("sms").addEventListener("click", SMS);
+document.getElementById("achats").addEventListener("click", Achats);
+document.getElementById("achatDeApp").addEventListener("click", achatApp);
+document.getElementById("politique").addEventListener("click", Politique);
+document.getElementById("virus").addEventListener("click", Virus);
+document.getElementById("attaques").addEventListener("click", Attaques);
+document.getElementById("mobiles").addEventListener("click", Mobiles);}
