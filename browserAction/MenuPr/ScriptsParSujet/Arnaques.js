4334ca62f88e8c6f412c113820917c5f7587b68b
CyrilleTanguay <e1974422@cmaisonneuve.qc.ca>
2023-03-08 12:33:44 -0500

    Modifications de la page d'option (Version 1.0 finale)

--- browserAction/MenuPr/ScriptsParSujet/Arnaques.js
+++ browserAction/MenuPr/ScriptsParSujet/Arnaques.js
@@ -9,7 +9,9 @@
  * 
  */
 function Arnaques() {
+   //On laisse les indexations restantes vides 
   indexation.splice(9,5);
+
   indexation[0] = "<p id='introP'>Une messagerie est un bon moyen de vous arnaquer. Il existe cependant des moyens pour vous en prémunir.</p>";
   indexation[1] = "<p id='ouvrirStrcArn' class='iconeOui'><span class='icon-FA-list-ol'></span>Structure<span class='icon-circle-down'></span></p><div id='structure' style='display: none'><ul><li>Elle contient des couleurs vives et des majuscules</li><li>Elle contient beaucoup de fautes d’orthographe et de syntaxe</li><li>L’adresse courriel est très douteuse</li><li>Ils sont généralistes.</li></ul></div><hr>"
   indexation[2] = "<p id='ouvrirContenuArn' class='iconeOui'><span class='icon-FA-list-ul'></span>Le contenu<span class='icon-circle-down'></span></p><div id='contenuEscr' style='display: none'><ul><li>Ils sont pressants</li><li>Ils contiennent une source crédible</li><li>Ils vous menacent si vous ne vous faites pas quelque chose rapidement.</li><li>Ils contiennent des liens et des pièces jointes suspicieuses</li><li>Ils vous invitent à partager un courriel.</li></ul></div><hr>";
@@ -19,11 +21,7 @@ function Arnaques() {
   indexation[6] = "<p id='phish' class='iconeOui'><span class='icon-FA-anchor'></span>Ingénierie sociale (ou phishing)<span class='icon-circle-down'></span></p><div id='phishing' style='display: none'><p id='introP'>Il s’agit probablement du type d’arnaque le plus populaire. Elle peut être:</p><ul><li>Une carte de vœux</li><li>Un contact institutionnel</li><li>Un site de rencontre (« Rencontrez ces filles célibataires dans votre région »)</li><li>Un message indiquant que vous avez gagné un prix</li><li>Une arnaque au tueur (un tueur à gage a été engagé pour vous assassiner, mais est prêt à vous oublier en échange d’une somme)</li><li>Un message indiquant que vous avez été piraté et qui vous demande de vous connecter à un site pour estimer les dégâts</li><li>Un message de l’équipe de support Microsoft, qui vous propose des conseils payants </li><li>Une fausse institution financière qui vous presse de transférer vos fonds.</li><li>Une urgence médicale d’un de vos contacts</li><li>Des dettes</li><li>Un message de la compagnie de téléphone</li><li>Un message avec une annonce trop belle pour être vraie</li><li>De la police (la police ne vous demandera jamais des infos sur votre carte de crédit)</li></ul></div><hr>"
   indexation[7] = "<p id='autArn' class='iconeOui'><span class='icon-FA-ellipsis-h'></span>Autres<span class='icon-circle-down'></span></p><div id='autresArn' style='display: none'>"
   indexation[8] = "<ul><li>On vous demande de faire un paiement via des moyens qui ne sont pas sécuritaires</li><li>Un contact vous demande de payer vers un nouveau compte</li><li>Un vendeur que vous avez déjà payé vous demande plus d'argent en raison de circonstances imprévues</li><li>L’arnaque « Hi mum » (littéralement « allô maman »): Quelqu’un se fait passer pour un de vos contacts et vous fait croire qu’il a perdu son téléphone. Ce message peut contenir un soi-disant « nouveau numéro de téléphone ». En cas de doute, appelez votre ami avec le numéro de téléphone d’origine et non celui du courriel.</li><li>*Un ami prétendument atteint d’un cancer vous demande d’acheter des cartes Google Play et dit qu’il va vous rembourser bientôt. Bien que l'adresse courriel correspond à celle de votre ami, elle reste une arnaque, <b><i><u>mais ne supprimez pas le message.</u></i></b> Cela signifie surtout que son compte a été piraté, alors appelez-la par téléphone pour l’avertir.</li><li>Quelqu’un vous envoie un message avec votre propre adresse courriel, prétendant qu’il a pris le contrôle de votre compte et vous demande de l’argent. Cette personne vous ment: tout le monde peut écrire un courriel à partir de l’adresse de n’importe qui. Si vous avez des doutes, regardez s’il y a des connections inconnues dans les paramètres.</li><li>Il peut s’agir d’une fausse commande, d’un faux prélèvement ou d’un faux concours.</li><li>Il s’agit d’une prescription de médicaments, de méthodes d’enrichissement, de services financiers/réduction de dettes, diplômes à acheter ou de jeux d’argent</li></ul></div>";
-  //On laisse les indexations restantes vides 
-  // for (let i = 9; i < 14; i++) {
-  //   indexation[i] = '';
-  // };
-  
+ 
   //On s'assure que les variables ne soient pas séparées par une virgule
   const text = indexation.join('');
   //On remplit la fenêtre avec ces catégories
